f568d76a87cf2039c9a6c255929437458df24d2b
Babak Taremi <babaktaremi@yahoo.com>
2024-01-02 19:00:30 +0330

    Added Masstransit Support

--- Kafka.Example.Producer/Program.cs
+++ Kafka.Example.Producer/Program.cs
@@ -4,6 +4,9 @@ using Kafka.Example.Producer;
 using Kafka.Example.Producer.Models;
 using Kafka.Example.Producer.Protos;
 using Kafka.Example.Producer.Services;
+using MassTransit;
+using MassTransit.KafkaIntegration;
+using MassTransit.Serialization;
 using Timestamp = Google.Protobuf.WellKnownTypes.Timestamp;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +22,23 @@ builder.Services.Configure<ProducerConfig>(config =>
 });
 
 builder.Services.AddSingleton(typeof(IKafkaProducerService<,,>), typeof(KafkaProducerService<,,>));
+
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.UsingInMemory();
+    configurator.AddRider(rider =>
+    {
+        rider.AddProducer<Null,OrderModel>("order-fake-topic", (x, s) =>
+        {
+            s.SetValueSerializer(new KafkaJsonSerializer<OrderModel>());
+        });
+
+        rider.UsingKafka((context, kafkaConfig) =>
+        {
+            kafkaConfig.Host("localhost:9092");
+        });
+    });
+});
 
 var app = builder.Build();
 
@@ -44,6 +64,22 @@ app.MapPost("/OrderSubmitter", async (IKafkaProducerService<Null
     foreach (var orderModel in fakeOrders)
     {
         await service.ProduceAsync("order-fake-topic", new Message<Null, OrderModel>() { Value = orderModel });
+    }
+});
+
+
+app.MapPost("/OrderSubmitterMassTransit", async (ITopicProducerProvider producer) =>
+{
+    var fakeOrders = new Faker<OrderModel>()
+        .RuleFor(c => c.OrderId, Guid.NewGuid)
+        .RuleFor(c => c.OrderName, f => f.Commerce.Product())
+        .Generate(10);
+
+    var messageProducer = producer.GetProducer<OrderModel>(new Uri("topic:order-fake-topic"));
+
+    foreach (var orderModel in fakeOrders)
+    {
+        await messageProducer.Produce(orderModel);
     }
 });
 
